5902656cee018a0da79859939dccc6114ad36912
Antonio Membrides Espinosa <tonykssa@gmail.com>
2024-03-14 15:55:55 +0100

    update

--- api/tag.js
+++ api/tag.js
@@ -1,5 +1,35 @@
 export default function (req, res) {
 	res.json({
-		name: "hola"
+		name: "*",
+		url: req.url,
+		method: req.method,
+		query: req.query,
+		param: req.param,
+		params: req.params,
+		body: req.body
+	})
+}
+
+export function create(req, res) {
+	res.json({
+		name: "create",
+		url: req.url,
+		method: req.method,
+		query: req.query,
+		param: req.param,
+		params: req.params,
+		body: req.body
+	})
+}
+
+export function list(req, res) {
+	res.json({
+		name: "list",
+		url: req.url,
+		method: req.method,
+		query: req.query,
+		param: req.param,
+		params: req.params,
+		body: req.body
 	})
 }
